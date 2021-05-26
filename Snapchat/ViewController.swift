f864e5aff7e9eb419599ac54dfe48aaf509ba1a9
Mariam Dalia Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2021-05-26 13:55:20 -0700

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Mariam Apaza on 5/26/21.
+//  Copyright © 2021 Mariam Apaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
